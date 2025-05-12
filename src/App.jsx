e3384e4b8c9ad6588d23815f044c4779ed88ae94
Amarjeev Ms <amarjeevm@gmail.com>
2025-05-12 15:40:42 +0530

    Nav-bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from 'react';
 
 function App() {
   return (
-    <div>
-      <h1>hjellooooo</h1><h1>ammu</h1>
+    <div data-theme="dark" className="min-h-screen p-4">
+      {/* Navbar */}
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">🤝 Dev Tinder</a>
+        </div>
+        <div className="flex gap-2">
+          <div className="dropdown dropdown-end mx-7">
+            <div 
+              tabIndex={0} 
+              role="button" 
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li><a>Settings</a></li>
+              <li><a>Logout</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      {/* Navbar End */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
